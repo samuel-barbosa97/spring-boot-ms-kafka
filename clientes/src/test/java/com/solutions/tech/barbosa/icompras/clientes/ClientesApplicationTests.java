43085c25581dde8910c0a1694f29a172ce949d60
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2025-08-22 13:20:57 -0300

    Criando projeto e subindos instancias dos bancos de dados

---
+++ clientes/src/test/java/com/solutions/tech/barbosa/icompras/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.tech.barbosa.icompras.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
